2812ce1817acff5cdea619bc7ef7b5b9430d3fc1
h_osholm <h_osholm@users.sf.net>
2008-08-18 15:05:24 +0000

    still playing with around

--- test/test_radial_divergence.py
+++ test/test_radial_divergence.py
@@ -21,7 +21,7 @@ U = n.array([-0.52880000,  0.78460000,  0.32380000, -0.51780000,  0.00400000, -0
 pos = [0.0, 0.0 , 0.0]
 
 
-hkl = [1,1,1]
+hkl = n.array([1,1,1])*3
 
 unit_cell = [4.04975, 4.04975, 4.04975, 90, 90, 90]
 B = tools.FormB(unit_cell)
@@ -29,24 +29,51 @@ Gc = n.dot(B,hkl)
 Gw = n.dot(U,Gc)
 
 
+wavedisp = 1e-3
+sigma_limit = 2.
 
 wavelength_0 = 0.4859292
+
 wavelength = wavelength_0 
 tth = tools.tth2(Gw,wavelength)
 (Omega, Eta) = tools.find_omega_wedge(Gw,tth,wedge)
+omega_0 = Omega[1]*180./n.pi
 Om = tools.OMEGA(Omega[1])
 Gt = n.dot(Om,Gw)
-
 (dety,detz) = detector.det_coor(Gt,n.cos(tth),wavelength,distance,y_size,z_size,dety_center,detz_center,n.identity(3),0,0,0)
 
-wavelength = wavelength_0*(1.0+1e-3)
-#wavelength = wavelength_0*(1.0+.1e-1)
+# Calculate detector position MINUS sigma_limit times sigma
+wavelength = wavelength_0*(1.0+wavedisp/2.*sigma_limit)
 tth = tools.tth2(Gw,wavelength)
 (Omega, Eta) = tools.find_omega_wedge(Gw,tth,wedge)
+omega_p = Omega[1]*180./n.pi
 Om = tools.OMEGA(Omega[1])
 Gt = n.dot(Om,Gw)
 
-(ddety,ddetz) = detector.det_coor(Gt,n.cos(tth),wavelength,distance,y_size,z_size,dety_center,detz_center,n.identity(3),0,0,0)
+(ddety_p,ddetz_p) = detector.det_coor(Gt,n.cos(tth),wavelength,distance,y_size,z_size,dety_center,detz_center,n.identity(3),0,0,0)
+
+# Calculate detector position PLUS sigma_limit times sigma
+wavelength = wavelength_0*(1.0-wavedisp/2.*sigma_limit)
+tth = tools.tth2(Gw,wavelength)
+(Omega, Eta) = tools.find_omega_wedge(Gw,tth,wedge)
+omega_n = Omega[1]*180./n.pi
+Om = tools.OMEGA(Omega[1])
+Gt = n.dot(Om,Gw)
+
+(ddety_n,ddetz_n) = detector.det_coor(Gt,n.cos(tth),wavelength,distance,y_size,z_size,dety_center,detz_center,n.identity(3),0,0,0)
+
+print 'Distance in pixels %f' %(n.sqrt((ddety_p-dety)*(ddety_p-dety)+(ddetz_p-detz)*(ddetz_p-detz)))
+print 'Distance in pixels %f' %(n.sqrt((ddety_n-dety)*(ddety_n-dety)+(ddetz_n-detz)*(ddetz_n-detz)))
+print 'Omage values, c, p, n:', omega_0, omega_p, omega_n
+
+from polyxsim import pixel_trace
+from scipy.stats import norm
+pixels =  pixel_trace.pixel_trace([ddety_n,ddetz_n,ddety_p,ddetz_p])
+pixels2 = n.concatenate(([[0,0,0.0]],pixels))
+cums = pixels2[:,2].cumsum()
+cdfcums = norm.cdf(cums-cums[-1]/2.0,scale = cums[-1]/(2*sigma_limit))
+frac =  cdfcums[1:]-cdfcums[:-1]
+
 
 ### CHANGE UNIT_CELL
 unit_cell = n.array([4.04975, 4.04975, 4.04975, 90, 90, 90])
@@ -63,7 +90,14 @@ Gt = n.dot(Om,Gw)
 
 (dddety,dddetz) = detector.det_coor(Gt,n.cos(tth),wavelength,distance,y_size,z_size,dety_center,detz_center,n.identity(3),0,0,0)
 
+from polyxsim import pixel_trace
+from scipy.stats import norm
+pixels2 = n.concatenate(([[0,0,0.0]],pixels))
 
+pixels2 = n.concatenate(([[0,0,0.0]],pixels))
+
+cums = pixels2[:,2].cumsum()
+cdfcums = norm.cdf(cums-cums[-1]/2.0)
 
 #### Playing with an algo for convolute the radial spread of the reflections
 from polyxsim import pixel_trace
