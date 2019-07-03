65caa7bffdc24f78eb3c35c6228c6a6704236b2e
Jonathan Wright <wright@esrf.fr>
2019-07-03 14:52:39 +0200

    fix for peakshape != 3

--- polyxsim/make_image.py
+++ polyxsim/make_image.py
@@ -167,8 +167,8 @@ class make_image:
                             for z in zrange:
                                 if y > 0 and y < framedimy and z > 0 and z < framedimz and abs(dety+frame_add-y) < 1 and abs(detz+frame_add-z) < 1:
 #                                   frame[y-1,z] = frame[y-1,z] + fraction*intensity*(1-abs(dety+frame_add-y))*(1-abs(detz+frame_add-z))
-                                    y = int(round(dety_present))
-                                    z = int(round(detz_present))
+                                    y = int(round(y))
+                                    z = int(round(z))
                                     frame[y,z] = frame[y,z] + fraction*intensity*(1-abs(dety+frame_add-y))*(1-abs(detz+frame_add-z))
 
             # 2D Gaussian on detector                   
